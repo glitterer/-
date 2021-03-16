d5d800adcf3202e0853b45bb91dbab1f195a3607
glitterer <50728605+glitterer@users.noreply.github.com>
2021-03-16 22:00:16 +0900

    Update 2675번_문자열 반복(in C).c

--- 백준/2675번_문자열 반복(in C).c
+++ 백준/2675번_문자열 반복(in C).c
@@ -6,7 +6,8 @@ int main()
 	char S[21];
 	scanf("%d", &T);
 	for(int i=0; i<T; i++){
-		scanf("%d %s", &R, &S);
+		scanf("%d", &R);
+		scanf("%s", &S)
 		for(int j=0; j<R; j++){
 				for (int k=0; k<R; k++){
 					printf("%c", S[j]);
