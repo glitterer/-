e60871cddd2fc178eb77008b62b90014726c7c5c
glitterer <50728605+glitterer@users.noreply.github.com>
2021-03-16 21:55:50 +0900

    Create 2675번_문자열 반복(in C).c

---
+++ 백준/2675번_문자열 반복(in C).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int T, R;
+	char S[21];
+	scanf("%d", &T);
+	for(int i=0; i<T; i++){
+		scanf("%d %s", &R, &S);
+		for(int j=0; j<R; j++){
+				for (int k=0; k<R; k++){
+					printf("%c", S[j]);
+			}
+		}printf("\n");
+	}
+	return 0;
+}
+
+//예시데로는 잘 나오는데 계속 틀렸다고 나옴...
